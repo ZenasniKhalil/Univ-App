e3dc8b516c43a38497b0c349cd270513eab183b6
Ibrahim El Khalil ZENASNI <zenasni.kha@gmail.com>
2023-04-03 16:09:24 +0100

    add login and groups and permission

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('ajouter-etudiants/', views.addStudent, name='add_student'),
     path('etudiants/', views.student, name='student'),
     path('sup-etudiants/', views.deleteStudents, name='sup_etudiants'),
-    path('ajouter-domaine/',views.addDomaine, name='ajouter-domaine')
+    path('ajouter-domaine/',views.addDomaine, name='ajouter-domaine'),
+    path('ajouter-filiere/',views.addFiliere, name='ajouter-filiere'),
 ]
--- base/views.py
+++ base/views.py
@@ -5,7 +5,9 @@ from django.contrib import messages
 from .forms import DomaineForm, FiliereForm
 from .filters import EtudiantFilter, VerifierFilter
 from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required, permission_required
 
+@login_required(login_url='/login/')
 def dashboard(request):
     table_header = ['Mat. Etudiant',
                     'Nom',
@@ -17,12 +19,16 @@ def dashboard(request):
                     'Date de l\'examen',
                     'Heure de l\'examen']
     records = VerifierFilter(request.GET, queryset=Verifier.objects.all())
+    print(request.user.has_perm("base.add_domaine"))
     context={
         'records':records,
-        'header':table_header
+        'header':table_header,
+        'admin': request.user.groups.filter(name='admins').exists(),
+        'stuff':request.user.groups.filter(name='stuff').exists(),
     }
     return render(request, 'base/records.html', context)
 
+@login_required(login_url='/login/')
 def student(request):
     table_header = ['Mat. Etudiant',
                     'Nom',
@@ -35,10 +41,13 @@ def student(request):
     context = {
         'students':students,
         'header':table_header,
+        'admin': request.user.groups.filter(name='admins').exists(),
+        'stuff':request.user.groups.filter(name='stuff').exists(),
     }
     return render(request, 'base/student.html',context)
 
-
+@login_required(login_url='/login/')
+@permission_required('base.add_etudiant')
 def addStudent(request):
     if request.method == 'POST':
         if request.FILES.get('myfile'):
@@ -50,13 +59,16 @@ def addStudent(request):
             messages.error(request, 'Vous n\'avez pas selction un fichier excel !!! ')
     return render(request, 'base/ajouter-etudiants.html')
 
+@login_required(login_url='/login/')
+@permission_required('base.delete_etudiant')
 def deleteStudents(request):
     if request.method == 'POST':
         Etudiant.objects.all().delete()
         return redirect('base:student')
     return render(request, 'base/sup_etudiants.html')
 
-
+@login_required(login_url='/login/')
+@permission_required('base.add_domaine')
 def addDomaine(request):
     form = DomaineForm()
     if request.method == 'POST':
@@ -73,6 +85,25 @@ def addDomaine(request):
         'form':form
     }
     return render(request, 'base/domaine.html',context)
+
+@login_required(login_url='/login/')
+@permission_required('base.add_filiere')
+def addFiliere(request):
+    form = FiliereForm()
+    if request.method == 'POST':
+        try:
+            Filiere.objects.create(
+                code=request.POST.get('code'),
+                nom=request.POST.get('nom')
+            )
+            return redirect('base:dashboard')
+        except IntegrityError as e:
+            messages.error(request, 'Le Code <strong>'+str(request.POST.get('code'))+'</strong> est deja existant !!!')
+        
+    context={
+        'form':form
+    }
+    return render(request, 'base/filiere.html',context)
 
 
 
